080c09c860b801fb7291010ea92c3ea3361f2ab7
louis7308 <jswa7308@naver.com>
2022-08-05 23:18:40 +0900

    :tada: :: [개인 프로젝트] 당근마켓 클론코딩

---
+++ src/main/java/com/example/daangnclone/DaangnCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.daangnclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaangnCloneApplication.class, args);
+    }
+
+}
