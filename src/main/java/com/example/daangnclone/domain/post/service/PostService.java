05f9451acb661c2b39aa728a7a3e43111145119a
louis7308 <jswa7308@naver.com>
2022-09-10 23:44:48 +0900

    :sparkles: :: PostService

---
+++ src/main/java/com/example/daangnclone/domain/post/service/PostService.java
@@ -0,0 +1,40 @@
+package com.example.daangnclone.domain.post.service;
+
+import com.example.daangnclone.domain.post.model.PostListRes;
+import com.example.daangnclone.domain.post.repository.ClipboardRepository;
+import com.example.daangnclone.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.Filter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+    private final ClipboardRepository clipboardRepository;
+
+
+    /**
+     *
+     * 중고 거래 물품 게시글 전체 리스트 조회
+     *
+     * @author jswa7
+     * @version 1.0.0
+     * 작성일 2022-08-23
+    **/
+    public List<PostListRes> findAll() {
+        List<PostListRes> posts = new ArrayList<>();
+        postRepository.findAll().forEach(p -> posts.add(PostListRes.builder()
+                        .id(p.getId())
+                        .title(p.getTitle())
+                        .category(p.getCategory())
+                        .price(p.getPrice())
+                        .isSuggested(p.getIsSuggested())
+                        .content(p.getContent())
+                .build()));
+        return posts;
+    }
+}
