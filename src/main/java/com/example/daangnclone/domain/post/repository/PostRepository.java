c864c0b4ca355a33e47826bdd8c9c5b5ed185d4c
louis7308 <jswa7308@naver.com>
2022-09-16 23:50:20 +0900

    :sparkles: :: postRepository

---
+++ src/main/java/com/example/daangnclone/domain/post/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.example.daangnclone.domain.post.repository;
+
+import com.example.daangnclone.domain.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
